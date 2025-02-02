97ba0781ee447437e12adc2bc45a885dc2e4afd2
Hemand Sunny <65974687+hemandsunny@users.noreply.github.com>
2025-02-02 19:38:59 +0530

    Create sum_diff.c

---
+++ basics/sum_diff.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+//hackerrank - sum and difference of 2 numbers
+int main()
+{
+    int a,b;float c,d;
+    scanf("%d %d\n",&a,&b);
+    scanf("%f %f\n",&c,&d);
+    printf("%d %d\n",(a+b),(a-b));
+    printf("%.1f %.1f\n",(c+d),(c-d));
+    return 0;
+    
+}
