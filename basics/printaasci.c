6fcb86f3c99316842ce28dc81d94d47b0955a84c
Hemand Sunny <hemandsunny@gmail.com>
2025-02-08 22:05:37 +0530

    aasci printer

---
+++ basics/printaasci.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+//print aascii value of a character
+
+void main()
+{
+    char a;
+    printf("enter a character : ");
+    scanf("%c",&a);
+    printf("aascii value of %c: %d\n",a,a);
+    return;
+}
